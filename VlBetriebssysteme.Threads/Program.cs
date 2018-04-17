fac131a4d4d6331cc7eda55b0d45c79d371d167b
Patrick Eberle <patrick.eberle@trivadis.com>
2018-04-17 13:15:07 +0200

    Added Threads-project.

---
+++ VlBetriebssysteme.Threads/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace VlBetriebssysteme.Threads
+{
+    class Program
+    {
+        static volatile int credit = 0;
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(ProcessThread1);
+            Thread thread2 = new Thread(ProcessThread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            // thread1.Join();
+            // thread2.Join();
+
+            System.Console.WriteLine($"Credit is: {credit}");
+            Console.ReadLine();
+        }
+        static void ProcessThread1()
+        {
+            credit += 20;        
+            System.Console.WriteLine($"Thread 1 | Credit has value: {credit}");
+        }
+
+        static void ProcessThread2()
+        {
+            credit += 21;
+            System.Console.WriteLine($"Thread 2 | Credit has value: {credit}");
+        }
+    }
+}
